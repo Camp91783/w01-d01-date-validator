bae3b7869f8a759ae2be1ae1fa37450e5631da2a
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-15 23:06:00 -0500

    reorganize / corrections

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -4,8 +4,13 @@ def valid_date(month, day, year)
   months_31 = [ 1, 3, 5, 7, 8, 10, 12]
   months_30 = [ 4, 6, 9, 11]
 
-  if month < 1 || month > 12
+  if month < 1 || month > 12    # make sure month between 1-12
   	return false
+end
+
+def valid_years ( years, months, days)  # valid years
+  if year < 1880 || year > 2280   
+        return false
 end
 
 def length_month(month, day)
@@ -73,8 +78,5 @@ def leap_month (month, day)
       end
     end
       
-Def valid_years ( years, months, days)
-  if year < 1880 || year > 2280   
-        return false
-end
+
 
