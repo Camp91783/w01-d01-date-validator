dbaa7ae002ffb98d876f7d9038e1aa6f95534166
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-16 17:24:56 -0500

    again and again trying to fix last line

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -73,5 +73,5 @@ def length_month (month, day)
 	end
 
 def leap_year(year)
-	year % 400 == 0 || (year % 4 == 0 && year % 100 !=0) 
-		end
+	if year % 400 == 0 || (year % 4 == 0 && year % 100 !=0) 
+	end
