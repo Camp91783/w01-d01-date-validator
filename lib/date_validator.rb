54f9eb095dbcb3e92f3373d93d7a087f80be122a
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-15 22:41:26 -0500

    add leap year and indent changes

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -16,45 +16,63 @@ end
 def length_month(month, day)
 	if month = 3 && day > 31
 			return false
-end
+  end
 
 def length_month(month, day)
 	if month = 5 &&	day > 31
 			return false
-end
+  end
 
 def length_month (month, day)
 	if month = 7 && day > 31
 			return false
+  end
 
 def length_month (month, day)
 	if month = 8 && day > 31 
 			return false
+  end
 
 def length_month (month, day)
 	if month = 10 && day > 31
 			return false
+  end
 
 def length_month (month, day)
 	if month = 12 && day > 31 
 	 		return false
+  end
 
 def length_month (month, day)
 	if month = 4 && day > 30
 		return false
+  end
 
 def length_month (month, day)
 	if month = 6 && day > 30
 		return false
+  end
 
 def length_month (month, day)
 	if month = 9 && day > 30
 		return false
+  end 
 
 def length_month (month, day)
 	if month = 11 && day > 30
 		return false
+	end
 
+def leap_month (month, day)
+	when month = 2 
+		if (year % 4 == 0 && year % 100 !=0) || year % 400 == 0
+			return false
+		else 
+			if day < 1 || day > 28
+				return false 
+      end
+    end
+      
 Def valid_years ( years, months, days)
   if year < 1880 || year > 2280   
         return false
