5a82fc7984d8984f9c007197d7103569523deed0
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-17 09:59:50 -0500

    day < 1 fix

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -23,60 +23,60 @@ def length_month(month, day)
 end	
 
 def length_month(month, day)
-	if month = 3 && day > 31
+	if month = 3 && day > 31 || day < 1
 			return false
 	end
 end
 
 def length_month(month, day)
-	if month = 5 &&	day > 31
+	if month = 5 &&	day > 31 || day < 1
 			return false
 	end
 end
 
 def length_month (month, day)
-	if month = 7 && day > 31
+	if month = 7 && day > 31 || day < 1
 			return false
 	end
 end	
 
 def length_month (month, day)
-	if month = 8 && day > 31 
+	if month = 8 && day > 31 || day < 1
 			return false
 	end
 end	
 
 def length_month (month, day)
-	if month = 10 && day > 31
+	if month = 10 && day > 31 || day < 1
 			return false
 	end
 end	
 
 def length_month (month, day)
-	if month = 12 && day > 31 
+	if month = 12 && day > 31 || day < 1
 	 		return false
 	end
 end	
 
 def length_month (month, day)
-	if month = 4 && day > 30
+	if month = 4 && day > 30 || day < 1
 		return false
 	end
 end
 
-def length_month (month, day)
+def length_month (month, day) day < 1
 	if month = 6 && day > 30
 		return false
 	end
 end
 
-def length_month (month, day)
+def length_month (month, day) day < 1
 	if month = 9 && day > 30
 		return false
 	end
 end 
 
-def length_month (month, day)
+def length_month (month, day) day < 1
 	if month = 11 && day > 30
 		return false
 	end
