b1654c314893b1a96875f67662cbefada0e6d635
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-17 09:56:49 -0500

    ,1

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,7 +1,7 @@
 def month_length(month, year)
     return 31 if [1,3,5,7,8,10,12].include(month)
     return 30 if [4,6,9,11].include(month)
-    return 29 if month == 2 && leap_year(year)
+    return 29 if month == 2 && leap_year(year) == true
     return 28 if month == 2 && leap_year(year) == false
  
   if month < 1 || month > 12    
@@ -17,7 +17,7 @@ def valid_year (year, leap_year)
 end
 
 def length_month(month, day)
-  if month = 1 && day > 31 
+  if month = 1 && day > 31 || day < 1
     	return false
 	end
 end	
