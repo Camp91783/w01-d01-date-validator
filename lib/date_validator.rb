bbc5eeb474a2c0a5ce15996808f149b7fbee3d0d
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-16 19:19:21 -0500

    final maybe?

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,77 +1,89 @@
-
-require "pry"
-
-def valid_date(month, day, year)
-  months_31 = [ 1, 3, 5, 7, 8, 10, 12]
-  months_30 = [ 4, 6, 9, 11]
-
+def month_length(month, year)
+    return 31 if [1,3,5,7,8,10,12].include(month)
+    return 30 if [4,6,9,11].include(month)
+    return 29 if month == 2 && leap_year?(year)
+    return 28 if month == 2 && leap_year?(year) == false
+ 
   if month < 1 || month > 12    
   		return false
 	end
-
-binding.pry
+end	
 
 
 def valid_years ( years, months, days)  
   if year < 1880 || year > 2280   
       return false
 	end
+end
 
 def length_month(month, day)
   if month = 1 && day > 31 
     	return false
 	end
+end	
 
 def length_month(month, day)
 	if month = 3 && day > 31
 			return false
 	end
+end
 
 def length_month(month, day)
 	if month = 5 &&	day > 31
 			return false
 	end
+end
 
 def length_month (month, day)
 	if month = 7 && day > 31
 			return false
 	end
+end	
 
 def length_month (month, day)
 	if month = 8 && day > 31 
 			return false
 	end
+end	
 
 def length_month (month, day)
 	if month = 10 && day > 31
 			return false
 	end
+end	
 
 def length_month (month, day)
 	if month = 12 && day > 31 
 	 		return false
 	end
+end	
 
 def length_month (month, day)
 	if month = 4 && day > 30
 		return false
 	end
+end
 
 def length_month (month, day)
 	if month = 6 && day > 30
 		return false
 	end
+end
 
 def length_month (month, day)
 	if month = 9 && day > 30
 		return false
-	end 
+	end
+end 
 
 def length_month (month, day)
 	if month = 11 && day > 30
 		return false
 	end
+end
+
 
 def leap_year(year)
-	if year % 400 == 0 || (year % 4 == 0 && year % 100 !=0) 
+		 year % 400 == 0 || (year % 4 == 0 && year % 100 !=0)
+end 
 
